60e09b0aa661c4f8077c869e65f594b13ec1645e
Simon Tokumine <simon@japancentre.com>
2009-04-03 14:05:27 +0100

    Added better question sort on home page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   layout "client_root"
   def index
-    @questions = Question.all :limit => 3, :order => "updated_at DESC"
+    @questions = Question.all :limit => 3, :order => "updated_at DESC" 
     @themes = Theme.all
     @map_data = MapDatum.all :limit => 8, :order => "created_at DESC"
   end
